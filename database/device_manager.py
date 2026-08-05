17051cf7a7a7bf70f560b63d860512869125d0ca
Mohammadmahdi Valizadeh <M.Valizadeh@respina.net>
2026-08-05 16:13:38 +0330

    Complete SQLite CRUD operations and add device lookup functions

---
+++ database/device_manager.py
@@ -0,0 +1,81 @@
+import sqlite3
+def add_device(name, ip):
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        INSERT INTO devices(name, ip)
+        VALUES (?, ?)
+        """,
+        (name, ip)
+    )
+    connection.commit()
+    connection.close()
+
+def get_all_devices():
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM devices")
+    devices = cursor.fetchall()
+    connection.close()
+    return devices
+
+def update_device(device_id, name, ip):
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        UPDATE devices
+        SET name = ?, ip = ?
+        WHERE id = ?
+        """,
+        (name, ip, device_id)
+    )
+    connection.commit()
+    connection.close()
+
+def delete_device(device_id):
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        DELETE FROM devices
+        WHERE id = ?
+        """,
+        (device_id,)
+    )
+    connection.commit()
+    connection.close()
+
+def get_device_by_ip(ip):
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        SELECT *
+        FROM devices
+        WHERE ip = ?
+        """,
+        (ip,)
+    )
+    device = cursor.fetchone()
+    connection.close()
+    return device
+
+def get_device_by_name(name):
+    connection = sqlite3.connect("netplus.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        SELECT *
+        FROM devices
+        WHERE name = ?
+        """,
+        (name,)
+    )
+    device = cursor.fetchone()
+    connection.close()
+    return device
+device = get_device_by_name("Core-R3")
+
+print(device)
